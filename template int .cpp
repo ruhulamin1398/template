ef7074a0d349c935013e2a39731471440a195d0f
ruhulaminrul <ruhul.ok@gmail.com>
2019-09-07 08:55:31 +0600

    line

---
+++ template int .cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll               int
+#define c(n)            scanf("%d",&n)
+#define c2(a,b)         scanf("%d%d",&a,&b)
+
+#define pl(n)            printf("%d\n",n)
+#define pls(n)           printf("%s\n",n)
+#define pl2(a,b)         printf("%d %d\n",a,b)
+
+#define p(n)            printf("%d",n)
+#define ps(n)           printf("%s",n)
+#define p2(a,b)         printf("%d %d",a,b)
+
+#define carray(n,arr)   for( ll i =1 ; i<=n ; i++)scanf("%d",arr+i)
+#define loop(n)         while (n--)
+#define tcase()         ll tc;scanf("%d",&tc);for( ll t = 1; t<=tc ; t++)
+#define MAX             250002
+
+
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ template long long .cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll              long long int
+#define c(n)            scanf("%lld",&n)
+#define c2(a,b)         scanf("%lld%lld",&a,&b)
+
+#define pl(n)            printf("%lld\n",n)
+#define pls(n)           printf("%s\n",n)
+#define pl2(a,b)         printf("%lld %lld\n",a,b)
+
+#define p(n)            printf("%lld",n)
+#define ps(n)           printf("%s",n)
+#define p2(a,b)         printf("%lld %lld",a,b)
+
+#define carray(n,arr)   for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
+#define loop(n)         while (n--)
+#define tcase()         ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
+#define MAX             250002
+
+
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
