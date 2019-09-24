72167ea73d34363f58d58cad30b79e9d3d5d9ddf
ruhulaminrul <43258231+ruhulaminrul@users.noreply.github.com>
2019-09-24 15:55:43 +0600

    Update template long long .cpp

--- template long long .cpp
+++ template long long .cpp
@@ -1,45 +1,110 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll              long long int
-#define MAX             250002
-
-#define tcase()         ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
-
-/**   scening     **/
-#define c(n)            scanf("%lld",&n)
-#define c2(a,b)         scanf("%lld%lld",&a,&b)
-#define c3(a,b,c)       scanf("%lld%lld%lld",&a,&b,&c)
+#define 	ll              	long long int
+#define 	MAX             	250002
+#define    	pi             	2.0*acos(0.0)
+#define    	mod1            1000000007 // prime
+#define    	mod2            1000000009 // prime
+#define    	mod3            1000000021 // prime
 
 
-/**   Scening array      **/
+#define    	flash           	ios_base::sync_with_stdio(false);cin.tie(0);
 
-#define carray(n,arr)   for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
-#define carray1(n,arr)  for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
+#define 	tcase()         	ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
+#define 	pcase()          	printf("Case %lld: ",t)
 
-
-/**   printing with line     **/
-
-#define pl(n)           printf("%lld\n",n)
-#define pls(n)          printf("%s\n",n)
-#define plc(n)          printf("%c\n",n)
-#define pl2(a,b)        printf("%lld %lld\n",a,b)
+													/**   scening     **/
+#define 	c(n)            	scanf("%lld",&n)
+#define 	c(a,b)         		scanf("%lld%lld",&a,&b)
+#define 	c(a,b,c)       		scanf("%lld%lld%lld",&a,&b,&c)
+#define 	c(a,b,c,d)      	scanf("%lld%lld%lld%lld",&a,&b,&c,&d)
 
 
+													/**   Scening array      **/
 
-/**   printing     **/
-#define p(n)            printf("%lld",n)
-#define ps(n)           printf("%s",n)
-#define pc(n)           printf("%c",n)
-#define p2(a,b)         printf("%lld %lld",a,b)
+#define 	carray(n,arr)   	for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
+#define 	carray1(n,arr)  	for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
 
 
+													/**   printing with line     **/
 
-/**   Loop     **/
+#define 	pl(n)           	printf("%lld\n",n)
+#define 	pl(a,b)        		printf("%lld %lld\n",a,b)
+#define 	pls(n)          	printf("%s\n",n)
+#define 	plc(n)          	printf("%c\n",n)
 
-#define loop(n)         while (n--)
-#define loop0(n)        for( ll i =0 ; i<n ; i++)
-#define loop1(n)        for( ll i =1 ; i<=n ; i++)
+
+
+													/**   printing     **/
+#define 	p(n)            	printf("%lld",n)
+#define 	p(a,b)         		printf("%lld %lld",a,b)
+#define 	ps(n)           	printf("%s",n)
+#define 	pc(n)           	printf("%c",n)
+
+
+
+													/**   Loop     **/
+
+#define 	loop(n)         	ll haha = n; while (haha--)
+#define 	loop0(n)        	for( ll i =0 ; i<n ; i++)
+#define 	loop1(n)        	for( ll i =1 ; i<=n ; i++)
+
+
+
+#define 	loopi(x) 			for(ll i = 0; i < x; ++i)
+#define 	loopj(x) 			for(ll j = 0; j < x; ++j)
+#define 	loopk(x) 			for(ll k = 0; k < x; ++k)
+#define 	loopl(x) 			for(ll l = 0; l < x; ++l)
+
+#define 	loop1i(x) 			for(ll  i = 1; i < =x; ++i)
+#define 	loop1j(x) 			for(ll  j = 1; j < =x; ++j)
+#define 	loop1k(x) 			for(ll  k = 1; k < =x; ++k)
+#define 	loop1l(x) 			for(ll  l = 1; l < =x; ++l)
+
+
+													/**   stl     **/
+#define 	pb              	push_back
+#define 	mp              	make_pair
+#define 	pll             	pair< ll , ll >
+#define 	ppll            	pair< ll , pair<ll,ll >>
+#define 	mll             	map< ll , ll >
+#define 	msl             	map< string , ll >
+#define 	mset            	multiset< ll>
+#define 	pq              	priority_queue<ll,vector<ll>,greater<ll> >
+
+
+
+
+
+#define    maxi(a,b)        	max({a,b})
+#define    maxi(a,b,c)      	max({a,b,c})
+#define    maxi(a,b,c,d)    	max({a,b,c,d})
+#define    mini(a,b)        	min({a,b})
+#define    mini(a,b,c)      	min({a,b,c})
+#define    mini(a,b,c,d)    	min({a,b,c,d})
+#define    gcd(a,b)        		__gcd(a,b)
+#define    lcm(a,b)        		(a*b)/__gcd(a,b)
+#define    mems(a,b)        	memset(a,b,sizeof a)
+
+
+#define    lbound(a,x)         (lower_bound(a.begin(),a.end(),x) - a.begin())
+#define    ubound(a,x)         (upper_bound(a.begin(),a.end(),x) - a.begin())
+
+
+
+
+
+
+//int dx[] ={-1,0, 0,1};
+//int dy[] ={ 0,1,-1,0};
+//int dx8[]={-1,-1,-1,0,1,1, 1, 0};
+//int dy8[]={-1, 0, 1,1,1,0,-1,-1};
+//int kx[] ={2,1,-1,-2,-2,-1, 1, 2};
+//int ky[] ={1,2, 2, 1,-1,-2,-2,-1};
+
+
+
 
 
 
