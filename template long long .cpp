8cca073f70e697582d053b793ddaefe3d32b7b74
ruhulaminrul <43258231+ruhulaminrul@users.noreply.github.com>
2019-09-24 14:42:52 +0600

    Update template long long .cpp

--- template long long .cpp
+++ template long long .cpp
@@ -1,29 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 #define ll              long long int
+#define MAX             250002
+
+
+
+#define tcase()         ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
+
+
+
 #define c(n)            scanf("%lld",&n)
 #define c2(a,b)         scanf("%lld%lld",&a,&b)
+#define c3(a,b,c)       scanf("%lld%lld%lld",&a,&b,&c)
 
-#define pl(n)            printf("%lld\n",n)
-#define pls(n)           printf("%s\n",n)
-#define pl2(a,b)         printf("%lld %lld\n",a,b)
+
+#define pl(n)           printf("%lld\n",n)
+#define pls(n)          printf("%s\n",n)
+#define pl2(a,b)        printf("%lld %lld\n",a,b)
+
+
 
 #define p(n)            printf("%lld",n)
 #define ps(n)           printf("%s",n)
 #define p2(a,b)         printf("%lld %lld",a,b)
 
-#define carray(n,arr)   for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
+
+
+#define loop0(n)        for( ll i =0 ; i<n ; i++)
+#define loop1(n)        for( ll i =1 ; i<=n ; i++)
+
+
+#define carray(n,arr)   for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
+#define carray1(n,arr)  for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
 #define loop(n)         while (n--)
-#define tcase()         ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
-#define MAX             250002
 
 
+int main() {
 
 
-
-
-int main()
-{
 
 
     return 0;
