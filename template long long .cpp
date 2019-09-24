4939b6a23782ccdf0af73ddb77f76ea639699bd4
ruhulaminrul <43258231+ruhulaminrul@users.noreply.github.com>
2019-09-24 16:38:54 +0600

    Update template long long .cpp

--- template long long .cpp
+++ template long long .cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 #define 	ll              	long long int
 #define 	MAX             	250002
-#define    	pi             	2.0*acos(0.0)
-#define    	mod1            1000000007 // prime
-#define    	mod2            1000000009 // prime
-#define    	mod3            1000000021 // prime
+#define    	pi             	    2.0*acos(0.0)
+#define    	mod1                1000000007 // prime
+#define    	mod2                1000000009 // prime
+#define    	mod3                1000000021 // prime
 
 
 #define    	flash           	ios_base::sync_with_stdio(false);cin.tie(0);
@@ -14,37 +14,39 @@ using namespace std;
 #define 	tcase()         	ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
 #define 	pcase()          	printf("Case %lld: ",t)
 
-													/**   scening     **/
-#define 	c(n)            	scanf("%lld",&n)
-#define 	c(a,b)         		scanf("%lld%lld",&a,&b)
-#define 	c(a,b,c)       		scanf("%lld%lld%lld",&a,&b,&c)
-#define 	c(a,b,c,d)      	scanf("%lld%lld%lld%lld",&a,&b,&c,&d)
+/**   scening     **/
+#define 	c1(n)            	scanf("%lld",&n)
+#define 	c2(a,b)         		scanf("%lld%lld",&a,&b)
+#define 	c3(a,b,c)       		scanf("%lld%lld%lld",&a,&b,&c)
+#define 	c4(a,b,c,d)      	scanf("%lld%lld%lld%lld",&a,&b,&c,&d)
 
 
-													/**   Scening array      **/
+/**   Scening array      **/
 
 #define 	carray(n,arr)   	for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
 #define 	carray1(n,arr)  	for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
 
 
-													/**   printing with line     **/
+/**   printing with line     **/
 
-#define 	pl(n)           	printf("%lld\n",n)
-#define 	pl(a,b)        		printf("%lld %lld\n",a,b)
+#define 	pl1(n)           	printf("%lld\n",n)
+#define 	pl2(a,b)        	printf("%lld %lld\n",a,b)
+#define 	pl3(a,b,c)        	printf("%lld %lld %lld\n",a,b,c)
 #define 	pls(n)          	printf("%s\n",n)
 #define 	plc(n)          	printf("%c\n",n)
 
 
 
-													/**   printing     **/
-#define 	p(n)            	printf("%lld",n)
-#define 	p(a,b)         		printf("%lld %lld",a,b)
+/**   printing     **/
+#define 	p1(n)            	printf("%lld",n)
+#define 	p2(a,b)         	printf("%lld %lld",a,b)
+#define 	p3(a,b,c)        	printf("%lld %lld %lld",a,b,c)
 #define 	ps(n)           	printf("%s",n)
 #define 	pc(n)           	printf("%c",n)
 
 
 
-													/**   Loop     **/
+/**   Loop     **/
 
 #define 	loop(n)         	ll haha = n; while (haha--)
 #define 	loop0(n)        	for( ll i =0 ; i<n ; i++)
@@ -57,13 +59,13 @@ using namespace std;
 #define 	loopk(x) 			for(ll k = 0; k < x; ++k)
 #define 	loopl(x) 			for(ll l = 0; l < x; ++l)
 
-#define 	loop1i(x) 			for(ll  i = 1; i < =x; ++i)
-#define 	loop1j(x) 			for(ll  j = 1; j < =x; ++j)
-#define 	loop1k(x) 			for(ll  k = 1; k < =x; ++k)
-#define 	loop1l(x) 			for(ll  l = 1; l < =x; ++l)
+#define 	loop1i(x) 			for(ll  i = 1; i <=x; ++i)
+#define 	loop1j(x) 			for(ll  j = 1; j <=x; ++j)
+#define 	loop1k(x) 			for(ll  k = 1; k <=x; ++k)
+#define 	loop1l(x) 			for(ll  l = 1; l <=x; ++l)
 
 
-													/**   stl     **/
+/**   stl     **/
 #define 	pb              	push_back
 #define 	mp              	make_pair
 #define 	pll             	pair< ll , ll >
@@ -77,12 +79,12 @@ using namespace std;
 
 
 
-#define    maxi(a,b)        	max({a,b})
-#define    maxi(a,b,c)      	max({a,b,c})
-#define    maxi(a,b,c,d)    	max({a,b,c,d})
-#define    mini(a,b)        	min({a,b})
-#define    mini(a,b,c)      	min({a,b,c})
-#define    mini(a,b,c,d)    	min({a,b,c,d})
+#define    max2(a,b)        	max({a,b})
+#define    max3(a,b,c)      	max({a,b,c})
+#define    max4(a,b,c,d)    	max({a,b,c,d})
+#define    min2(a,b)        	min({a,b})
+#define    min3(a,b,c)      	min({a,b,c})
+#define    min4(a,b,c,d)    	min({a,b,c,d})
 #define    gcd(a,b)        		__gcd(a,b)
 #define    lcm(a,b)        		(a*b)/__gcd(a,b)
 #define    mems(a,b)        	memset(a,b,sizeof a)
@@ -95,24 +97,27 @@ using namespace std;
 
 
 
+/**Direction**/
 
-//int dx[] ={-1,0, 0,1};
-//int dy[] ={ 0,1,-1,0};
-//int dx8[]={-1,-1,-1,0,1,1, 1, 0};
-//int dy8[]={-1, 0, 1,1,1,0,-1,-1};
-//int kx[] ={2,1,-1,-2,-2,-1, 1, 2};
-//int ky[] ={1,2, 2, 1,-1,-2,-2,-1};
+//  ll dx4[] ={-1,0, 0,1};
+//  ll dy4[] ={ 0,1,-1,0};
+//  ll dx8[]={-1,-1,-1,0,1,1, 1, 0};
+//  ll dy8[]={-1, 0, 1,1,1,0,-1,-1};
+///Knight Direction
+//  ll kx[] ={2,1,-1,-2,-2,-1, 1, 2};
+//  ll ky[] ={1,2, 2, 1,-1,-2,-2,-1};
+
+///Hexagonal Direction
+//int dx[]={-1,-1,+0,+1,+1,+0};
+//int dy[]={-1,+1,+2,+1,-1,-2};
 
 
 
-
-
+/******************************Ajaira Jinish Sesh******************************/
 
 
 int main() {
 
-
-
-
-    return 0;
+	
+	return 0;
 }
