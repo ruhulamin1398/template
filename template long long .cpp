c281edbd7fc3c0e3a9eec11e1fc862952b0a108b
ruhulaminrul <43258231+ruhulaminrul@users.noreply.github.com>
2019-09-24 14:47:34 +0600

    Update template long long .cpp

--- template long long .cpp
+++ template long long .cpp
@@ -4,36 +4,44 @@ using namespace std;
 #define ll              long long int
 #define MAX             250002
 
-
-
 #define tcase()         ll tc;scanf("%lld",&tc);for( ll t = 1; t<=tc ; t++)
 
-
-
+/**   scening     **/
 #define c(n)            scanf("%lld",&n)
 #define c2(a,b)         scanf("%lld%lld",&a,&b)
 #define c3(a,b,c)       scanf("%lld%lld%lld",&a,&b,&c)
 
 
+/**   Scening array      **/
+
+#define carray(n,arr)   for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
+#define carray1(n,arr)  for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
+
+
+/**   printing with line     **/
+
 #define pl(n)           printf("%lld\n",n)
 #define pls(n)          printf("%s\n",n)
+#define plc(n)          printf("%c\n",n)
 #define pl2(a,b)        printf("%lld %lld\n",a,b)
 
 
 
+/**   printing     **/
 #define p(n)            printf("%lld",n)
 #define ps(n)           printf("%s",n)
+#define pc(n)           printf("%c",n)
 #define p2(a,b)         printf("%lld %lld",a,b)
 
 
 
+/**   Loop     **/
+
+#define loop(n)         while (n--)
 #define loop0(n)        for( ll i =0 ; i<n ; i++)
 #define loop1(n)        for( ll i =1 ; i<=n ; i++)
 
 
-#define carray(n,arr)   for( ll i =0 ; i<n ; i++)scanf("%lld",arr+i)
-#define carray1(n,arr)  for( ll i =1 ; i<=n ; i++)scanf("%lld",arr+i)
-#define loop(n)         while (n--)
 
 
 int main() {
